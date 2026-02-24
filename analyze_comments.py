20f06bc4709fd5dfe4d2043d400617ed35886927
MahdiBahar <mahdi.bahar.g@gmail.com>
2026-02-24 18:31:43 +0330

    analyze comments  from dima_comments when ai_title in dima_comments_analysis is not null

--- analyze_comments.py
+++ analyze_comments.py
@@ -15,23 +15,50 @@ def fetch_comments_to_analyze():
         cursor = conn.cursor()
 
         query = """
-            SELECT
-                id,
-                description,
-                sentiment_result,
-                created_at,
-                title
-            FROM dima_comments
-            WHERE
-                is_repetitive IS FALSE
-                AND description IS NOT NULL 
-                AND TRIM(description) != ''
-                AND sentiment_score IS NOT NULL
-                AND created_at > '2026-02-06'
-                
-            ORDER BY id ASC
             
+
+                SELECT
+                c.id,
+                c.description,
+                c.sentiment_result,
+                c.created_at,
+                c.title
+            FROM dima_comments c
+            WHERE
+                c.is_repetitive IS FALSE
+                AND c.description IS NOT NULL 
+                AND TRIM(c.description) != ''
+                AND c.sentiment_score IS NOT NULL
+                AND NOT EXISTS (
+                    SELECT 1
+                    FROM dima_comments_analysis a
+                    WHERE a.comment_id = c.id
+                    AND a.ai_title IS NOT NULL
+                )
+            ORDER BY c.id Desc;
+
         """
+
+        # SELECT
+        #         id,
+        #         description,
+        #         sentiment_result,
+        #         created_at,
+        #         title
+        #     FROM dima_comments
+        #     WHERE
+        #         is_repetitive IS FALSE
+        #         AND description IS NOT NULL 
+        #         AND TRIM(description) != ''
+        #         AND sentiment_score IS NOT NULL
+        #         AND created_at > '2026-02-07'
+        #     ORDER BY id ASC
+            
+        #  AND created_at <= '2026-12-25' AND created_at >= '2025-02-05'
+            #        -- AND (id = 513720 OR id = 545508 OR id = 473709 OR id = 501915 OR id = 502773 OR id = 537167 OR id = 502668 OR id = 511481 OR id = 507888 OR id = 503077 OR id = 536059)
+            #    -- AND (id = 515503 OR id = 501153 OR id = 489193 OR id = 492679 OR id = 494628 OR id = 502342 OR id = 519966 OR id = 516450 OR id = 474548 OR id = 529284 OR id = 541840)
+            #     AND (id = 539833 OR id = 533841 OR id = 517043 OR id = 530853 OR id = 545238 OR id = 492321 OR id = 533903 OR id = 535596 OR id = 543121 OR id = 493901 OR id = 506171)
+        
 # AND is_analyzed IS FALSE
 # AND created_at >= '2026-01-25'
 #AND (id = 540486 OR id = 537365 OR id = 533010 OR id = 527713 OR id = 526221 OR id = 523844 OR id = 517013 OR id = 520234)
